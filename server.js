b24e63decfae3e5291244d2819b2d76f6c0c2adf
Abby Benjamin <ablizben@gmail.com>
2020-10-03 09:30:11 -0400

    deploying again

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/online-offline-budget-tracker0',
+  process.env.MONGODB_URI || 'mongodb://localhost/budgettracker',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
